16029379ee8a4786fc3665ea0c0a27c25ff27f59
Jacky <email.com>
2018-01-11 19:30:40 +1100

    Initial commit

---
+++ Chapter12 Exercises/Exercise1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12_Exercises
+{
+    class Exercise1
+    {
+        public static void Run(string[] args)
+        {
+            const string END = "end";
+            string temp = "";
+           
+            while(temp != "end")
+            {
+                Console.WriteLine("Enter folder name >> ");
+                temp = Console.ReadLine();
+
+
+            }
+        }
+    }
+}
---
+++ Chapter12 Exercises/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chapter12_Exercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Exercise1.Run(args);
+        }
+    }
+}
